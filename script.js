e3771e5c06872b9b8e7623901ebabbdc8adb41f8
amine <hmidaali676@gmail.com>
2025-03-31 16:46:47 +0000

    change the names of the icons to the corresponding images in the api response documentation

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ var geocoding_Url ="http://api.openweathermap.org/geo/1.0/direct?";
 var city_tag ="q=",limit ="&limit="+"1";
 // global var
 var appid ="&appid="+apiKey;
-var lon_response , lat_response;
+var  lat_response,lon_response ;
 /**
  * open weather app part
  */
@@ -15,6 +15,7 @@ var lon_response , lat_response;
 var openWeather_api_url ="https://api.openweathermap.org/data/2.5/weather?";
 var lat_tag = "lat=";
 var lon_tag ="&lon=";
+var unit_temp ="&units=metric";
 async function fetch_weather_data(){
     /**
      * make the api request to fetch the lat and longitude of the city required
@@ -23,17 +24,32 @@ async function fetch_weather_data(){
     let fullApiUrl_geocoding = `${geocoding_Url}${city_tag}${cityName}${limit}${appid}`;
     const geo_response = await fetch(fullApiUrl_geocoding);
     var geo_data = await geo_response.json();
-    
-    lon_response=geo_data[0].lon;
+
     lat_response=geo_data[0].lat;
+    lon_response=geo_data[0].lon;
+    
     console.log(lon_response, lat_response);
     /**
      * fetch the weather data
      */
-    let fullApiUrl_openWeatherApp =`${openWeather_api_url}${lat_tag}${lat_response}${lon_tag}${lon_response}${appid}`;
+    let fullApiUrl_openWeatherApp =`${openWeather_api_url}${lat_tag}${lat_response}${lon_tag}${lon_response}${unit_temp}${appid}`;
     const openWeather_response = await fetch(fullApiUrl_openWeatherApp);
     var openweatherApp_data = await openWeather_response.json();
     console.log(openweatherApp_data);
-    
+    /**
+     * update the page data:
+     */
+    document.querySelector(".city").innerHTML = openweatherApp_data.name;
+    document.querySelector(".temperature").innerHTML=Math.round(openweatherApp_data.main.temp) + "°C";
+    document.querySelector(".humidity").innerHTML = openweatherApp_data.main.humidity +"%";
+    document.querySelector(".wind").innerHTML= openweatherApp_data.wind.speed +"Km/h";
+
+    /**
+     * update the images 
+     */
+    icon_name = openweatherApp_data.weather[0].icon;
+    icon_name = icon_name.replace("n","d");
+    let icon = document.querySelector(".weather_icon");
+    icon.src = `resources/images/${icon_name}.png`;
 
 }
